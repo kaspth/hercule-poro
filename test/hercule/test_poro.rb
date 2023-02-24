af65dd1fd1f3d8e399f9863cabbf8dadf77adfcf
Kasper Timm Hansen <kaspth@gmail.com>
2023-02-24 18:56:48 +0100

    Pretend to be professional about this and add tests + fancify test script

--- test/hercule/test_poro.rb
+++ test/hercule/test_poro.rb
@@ -3,7 +3,12 @@
 require "test_helper"
 
 class Hercule::TestPORO < Minitest::Test
-  def test_that_it_has_a_version_number
+  def test_version_number_exists
     refute_nil ::Hercule::PORO::VERSION
+  end
+
+  def test_inheritance
+    klass = Class.new Hercule::PORO
+    assert_equal Hercule::PORO, klass.superclass
   end
 end
